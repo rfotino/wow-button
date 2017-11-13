1652cc72b2e41ebfeafca5ed0263a638d1a5edec
Robert Fotino <robert.fotino@gmail.com>
2017-11-12 19:36:14 -0800

    Clips audio better and hopefully improves stopping

--- wow.js
+++ wow.js
@@ -1,42 +1,54 @@
 function init() {
   let sounds = [];
-  let playingSound = null;
+  let prevIndex = -1;
 
   for (let i = 1; i <= 7; i++) {
     let audio = new Audio();
-    audio.src = 'sounds/wow' + i + '.mp3';
+    let src = 'sounds/wow' + i + '.mp3';
+    audio.src = src;
     audio.preload = 'auto';
-    let callback =  () => {
+    let loadedCallback =  () => {
       console.log('loaded ' + audio.src);
       sounds.push(audio);
-      audio.removeEventListener('canplaythrough', callback);
+      audio.removeEventListener('canplaythrough', loadedCallback);
     };
-    audio.addEventListener('canplaythrough', callback);
-    audio.addEventListener('ended', () => {
-	console.log('ending ' + audio.src);
-	playingSound = null;
-    });
+    audio.addEventListener('canplaythrough', loadedCallback);
+    audio.stop = () => {
+      audio.src = (
+        'data:audio/wav;base64,' +
+        'UklGRiQAAABXQVZFZm10IBAAAAABAAEAVFYAAFRWAAABAAgAZGF0YQAAAAA='
+      );
+    };
+    audio.start = () => {
+      audio.src = src;
+      audio.play();
+    }
+    document.body.appendChild(audio);
   }
 
   let playSound = () => {
-    if (null !== playingSound) {
-      console.log('stopping ' + playingSound.src);
-      playingSound.currentTime = 0;
-      playingSound.pause();
-      playingSound = null;
+    // Stop any currently playing sounds
+    for (let sound of sounds) {
+      sound.stop();
     }
+    // Pick and play a random sound if one has loaded
     if (0 == sounds.length) {
       console.log('no loaded sounds to play yet!');
     } else {
-      let index = Math.floor(Math.random() * sounds.length);
-      playingSound = sounds[index];
-      console.log('playing ' + playingSound.src);
-      playingSound.play();
+      // Select a sound that is not the most recently played sound
+      let index;
+      do {
+        index = Math.floor(Math.random() * sounds.length);
+      } while (index === prevIndex);
+      prevIndex = index;
+      let sound = sounds[index];
+      console.log('playing ' + sound.src);
+      sound.start();
     }
   };
 
   let btn = document.getElementById('button');
-  for (let event of ['mousedown', 'touchstart']) {
+  for (let event of ['click', 'touchend']) {
     btn.addEventListener(event, playSound);
   }
 }
